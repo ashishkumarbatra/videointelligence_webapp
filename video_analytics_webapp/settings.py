d3144b7eb2ed06e6c29d44a3e07a0b588922cbb7
Ashish Batra <ashishbatra.ipec@gmail.com>
2018-06-18 19:38:54 +0530

    resolved disallowed host

--- video_analytics_webapp/settings.py
+++ video_analytics_webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '3t$qmj+9=&3smkc*pmv&%$by2he+ud+gwu(ijdysy99h0e)*)z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zee-poc-growth-solutions.appspot.com','127.0.0.1']
 
 
 # Application definition
