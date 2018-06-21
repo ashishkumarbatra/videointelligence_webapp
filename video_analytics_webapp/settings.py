fe583953608f3c9149f4ae61318ac0aeb4e1022e
Ashish Batra <ashishbatra.ipec@gmail.com>
2018-06-21 10:43:05 +0530

    fix for external IP

--- video_analytics_webapp/settings.py
+++ video_analytics_webapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '3t$qmj+9=&3smkc*pmv&%$by2he+ud+gwu(ijdysy99h0e)*)z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zee-poc-growth-solutions.appspot.com','127.0.0.1']
+ALLOWED_HOSTS = ['zee-poc-growth-solutions.appspot.com','127.0.0.1','35.188.200.138']
 
 
 # Application definition
